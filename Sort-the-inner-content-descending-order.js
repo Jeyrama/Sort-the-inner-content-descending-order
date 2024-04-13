aa44abdf2c50d9d6458bc5895ba15acfb1fe436f
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-04-12 20:33:39 -0700

    Create Sort-the-inner-content-descending-order.js

---
+++ Sort-the-inner-content-descending-order.js
@@ -0,0 +1,12 @@
+/*
+You have to sort the inner content of every word of a string in descending order.
+The inner content is the content of a word without first and the last char.
+
+Some examples:
+  "sort the inner content in descending order"  -->  "srot the inner ctonnet in dsnnieedcg oredr"
+  "wait for me"        -->  "wiat for me"
+  "this problem is easy"  -->  "tihs pboerlm is esay"
+
+Words are made up of lowercase letters.
+The string will never be null and will never be empty.
+*/
